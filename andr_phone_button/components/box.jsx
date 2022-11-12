75851957bf7b3a106b9e5748d051426a30f898d9
Tsogt2.0 <tsogt2.0@Nest38s-iMac.local>
2022-11-12 12:17:51 +0800

    react native init

---
+++ andr_phone_button/components/box.jsx
@@ -0,0 +1,13 @@
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+  } from 'react-native';
+
+export const Box = () =>{
+    return <Text>Helllo this is box</Text>
+}
